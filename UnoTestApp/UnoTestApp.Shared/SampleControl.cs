fc9d10930f71cd71f63076121c4a201e6376edae
Julie Cantin <julie.cantin@nventive.com>
2020-08-03 13:09:46 -0400

    reproducing the bug

---
+++ UnoTestApp/UnoTestApp.Shared/SampleControl.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+
+namespace UnoTestApp
+{
+	public class SampleControl : Control
+	{
+		public DataTemplate SampleTemplate
+		{
+			get { return (DataTemplate)GetValue(SampleTemplateProperty); }
+			set { SetValue(SampleTemplateProperty, value); }
+		}
+
+		public static readonly DependencyProperty SampleTemplateProperty =
+			DependencyProperty.Register("SampleTemplate", typeof(DataTemplate), typeof(SampleControl), new PropertyMetadata(null));
+
+		protected override void OnApplyTemplate()
+		{
+			base.OnApplyTemplate();
+			VisualStateManager.GoToState(this, "DisplayContent", false);
+		}
+	}
+}
