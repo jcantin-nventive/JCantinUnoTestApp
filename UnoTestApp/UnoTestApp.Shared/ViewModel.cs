4578acc5133efef6a834ca71e8916fe732ee6f51
Julie Cantin <julie.cantin@nventive.com>
2020-10-16 15:39:36 -0400

    works well

---
+++ UnoTestApp/UnoTestApp.Shared/ViewModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Windows.UI.Xaml.Data;
+
+namespace UnoTestApp
+{
+	[Bindable]
+	public class ViewModel : System.ComponentModel.INotifyPropertyChanged
+	{
+		public event System.ComponentModel.PropertyChangedEventHandler PropertyChanged;
+
+		private double _relativeOffset;
+		public double RelativeOffset
+		{
+			get => _relativeOffset;
+			set
+			{
+				_relativeOffset = value;
+				PropertyChanged?.Invoke(this, new System.ComponentModel.PropertyChangedEventArgs(nameof(RelativeOffset)));
+			}
+		}
+	}
+}
---
+++ UnoTestApp/UnoTestApp.Shared/ZeroToCustomConverter.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Windows.UI.Xaml.Data;
+
+namespace UnoTestApp
+{
+	public class ZeroToCustomConverter : IValueConverter
+	{
+		public object ZeroValue { get; set; }
+
+		public object NonZeroValue { get; set; }
+
+		public object Convert(object value, Type targetType, object parameter, string language)
+		{
+			if (value != null
+				&& (double)value > 0d)
+			{
+				return NonZeroValue;
+			}
+
+			return ZeroValue;
+		}
+
+		public object ConvertBack(object value, Type targetType, object parameter, string language)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
