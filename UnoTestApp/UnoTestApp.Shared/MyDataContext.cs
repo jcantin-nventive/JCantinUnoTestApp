e2e43877a34ab5a18267954b3a2406698da47e95
Julie Cantin <julie.cantin@nventive.com>
2020-11-02 14:57:00 -0500

    not bugged

---
+++ UnoTestApp/UnoTestApp.Shared/MyDataContext.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+
+namespace UnoTestApp
+{
+	[Windows.UI.Xaml.Data.Bindable]
+	public class MyDataContext : INotifyPropertyChanged
+	{
+		private bool _isTextBoxEnabled = true;
+
+		public MyDataContext()
+		{
+			var a = new BackgroundWorker();
+			a.DoWork += A_DoWork;
+			a.RunWorkerAsync();
+		}
+
+		private void A_DoWork(object sender, DoWorkEventArgs e)
+		{
+			_isTextBoxEnabled = false;
+			PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(IsTextBoxEnabled)));
+		}
+
+		public bool IsTextBoxEnabled => _isTextBoxEnabled;
+
+		public event PropertyChangedEventHandler PropertyChanged;
+	}
+}
